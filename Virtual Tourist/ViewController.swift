f04f97336617ef8e8d6852cd8b3a55b97b6654a6
Rodrigo N <rodrigocorcino@gmail.com>
2018-10-02 22:58:10 -0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Rodrigo on 02/10/18.
+//  Copyright © 2018 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

313828b8395df9901b81a6d9b4bd6b795809c64b
Rodrigo N <rodrigocorcino@gmail.com>
2018-10-05 00:56:13 -0300

    adjust map zoom

--- Virtual Tourist/DetailViewController.swift
+++ Virtual Tourist/DetailViewController.swift
@@ -13,14 +13,79 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     var annotation: MKAnnotation!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
+       
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(close))
+       // self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: @selector(close:))
+        
         // Do any additional setup after loading the view.
+        
+//        annotation.canShowCallout = true
+        
+        
+        guard let annotation = annotation else {
+            
+            return
+        }
+
+        
+        
+    
+        
         self.mapView .addAnnotation(annotation)
+        let latitude:CLLocationDegrees =  annotation.coordinate.latitude//insert latitutde
+        
+        let longitude:CLLocationDegrees = annotation.coordinate.longitude//insert longitude
+        
+        
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        
+        let reg = MKCoordinateRegion(center: location, latitudinalMeters: 500000, longitudinalMeters: 500000)
+       // let region = MKCoordinateRegion(center: location, span: span)
+        
+         mapView.setRegion(reg, animated: true)
+        
+//
+//        CLLocationCoordinate2D noLocation;
+//        MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 500, 500);
+//        MKCoordinateRegion adjustedRegion = [self.mapView regionThatFits:viewRegion];
+//        [self.mapView setRegion:adjustedRegion animated:YES];
+//        let latitude:CLLocationDegrees =  annotation.coordinate.latitude//insert latitutde
+//
+//        let longitude:CLLocationDegrees = annotation.coordinate.longitude//insert longitude
+//
+//        let latDelta:CLLocationDegrees = 0.05
+//
+//        let lonDelta:CLLocationDegrees = 0.05
+//
+//        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+//
+//        let location = CLLocationCoordinate2DMake(latitude, longitude)
+//
+//        let region = MKCoordinateRegion(center: location, span: span)
+//
+//       // self.mapView.showsUserLocation = true
+//
+//
+//        mapView.setRegion(region, animated: false)
+        
+    }
+    
+    
+    @IBAction func close(){
+        self.dismiss(animated: true, completion: nil)
     }
     
 
+    
     /*
     // MARK: - Navigation
 
--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -12,12 +12,36 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mapView.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            self.mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        }else{
+            locationManager.requestWhenInUseAuthorization()
+        }
+       
         // Do any additional setup after loading the view.
     }
+    
+
     
     
     @IBAction func addNewLocation(_ sender: UILongPressGestureRecognizer) {
@@ -30,8 +54,8 @@ class MapViewController: UIViewController {
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
-        annotation.title = "teste"
-        annotation.subtitle = "d¬_¬b"
+
+        
         self.mapView.addAnnotation(annotation)
     }
 
@@ -39,8 +63,11 @@ class MapViewController: UIViewController {
         if (segue.identifier == "segueDetailViewController") {
             if let navigation = segue.destination as? UINavigationController {
                 if let detail = navigation.viewControllers.first as? DetailViewController {
+                    detail.modalTransitionStyle = .crossDissolve
+                    if let annotation = sender as? MKAnnotation {
+                        detail.annotation = annotation
+                    }
                     
-                    detail.annotation = (sender as! MKAnnotation)
                 }
             }
         }
@@ -58,4 +85,13 @@ extension MapViewController: MKMapViewDelegate {
         self.performSegue(withIdentifier: "segueDetailViewController", sender: view.annotation)
     }
     
+    
 }
+
+extension MapViewController: CLLocationManagerDelegate {
+ 
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView .setCenter(userLocation.coordinate, animated: true)
+    }
+}
+
